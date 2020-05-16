3ffe537ef8f62fd865ebdcddfbcded632140e499
KKKK <36650689+atikapane@users.noreply.github.com>
2020-05-16 13:03:49 +0700

    Create grid layout for Home Page buttons
    Pemanggilan id masih pake findViewById

--- app/src/main/java/com/lessthanthree/anything/ui/home/HomeFragment.kt
+++ app/src/main/java/com/lessthanthree/anything/ui/home/HomeFragment.kt
@@ -8,11 +8,14 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.lessthanthree.anything.R
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+    private lateinit var rvhome :RecyclerView
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -22,10 +25,13 @@ class HomeFragment : Fragment() {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
+        rvhome = root.findViewById(R.id.rvHome)
+        rvhome.setHasFixedSize(true)
+        rvhome.layoutManager = GridLayoutManager(this.context, 2)
+
+        //tambahin manggil adapter di sini
+
         return root
     }
 }
