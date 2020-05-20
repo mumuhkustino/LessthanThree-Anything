902f077ed07cde4a2bdcbd2a9d444a3d4a05685b
C-70 <mumuhkustino@gmail.com>
2020-05-20 11:07:16 +0700

    Add button for Calculator

--- app/src/main/java/com/lessthanthree/anything/ui/calculator/CalculatorFragment.kt
+++ app/src/main/java/com/lessthanthree/anything/ui/calculator/CalculatorFragment.kt
@@ -34,24 +34,97 @@ class CalculatorFragment : Fragment() {
         // TODO: Use the ViewModel
         btnDivide.setOnClickListener {
             Toast.makeText(context, btnDivide.text.toString(), Toast.LENGTH_SHORT).show()
+            calculatorViewModel.divide()
+            textViewResult.text = btnDivide.text.toString()
         }
         btnMultiply.setOnClickListener {
             Toast.makeText(context, btnMultiply.text.toString(), Toast.LENGTH_SHORT).show()
+            calculatorViewModel.multiply()
+            textViewResult.text = btnMultiply.text.toString()
         }
         btnSubtraction.setOnClickListener {
             Toast.makeText(context, btnSubtraction.text.toString(), Toast.LENGTH_SHORT).show()
+            calculatorViewModel.subtraction()
+            textViewResult.text = btnSubtraction.text.toString()
         }
         btnAddition.setOnClickListener {
             Toast.makeText(context, btnAddition.text.toString(), Toast.LENGTH_SHORT).show()
+            calculatorViewModel.addition()
+            textViewResult.text = btnAddition.text.toString()
         }
         btnClear.setOnClickListener {
             Toast.makeText(context, btnClear.text.toString(), Toast.LENGTH_SHORT).show()
+            calculatorViewModel.clear()
+            textViewResult.text = btnClear.text.toString()
         }
         btnDelete.setOnClickListener {
             Toast.makeText(context, btnDelete.text.toString(), Toast.LENGTH_SHORT).show()
+            calculatorViewModel.delete()
+            textViewResult.text = btnDelete.text.toString()
         }
         btnEqual.setOnClickListener {
             Toast.makeText(context, btnEqual.text.toString(), Toast.LENGTH_SHORT).show()
+            textViewResult.text = calculatorViewModel.calculate()
+        }
+        btnNum0.setOnClickListener {
+            Toast.makeText(context, btnNum0.text.toString(), Toast.LENGTH_SHORT).show()
+            calculatorViewModel.setOperand(btnNum0.text.toString())
+            textViewResult.text = btnNum0.text.toString()
+        }
+        btnNum00.setOnClickListener {
+            Toast.makeText(context, btnNum00.text.toString(), Toast.LENGTH_SHORT).show()
+            calculatorViewModel.setOperand(btnNum00.text.toString())
+            textViewResult.text = btnNum00.text.toString()
+        }
+        btnNum1.setOnClickListener {
+            Toast.makeText(context, btnNum1.text.toString(), Toast.LENGTH_SHORT).show()
+            calculatorViewModel.setOperand(btnNum1.text.toString())
+            textViewResult.text = btnNum1.text.toString()
+        }
+        btnNum2.setOnClickListener {
+            Toast.makeText(context, btnNum2.text.toString(), Toast.LENGTH_SHORT).show()
+            calculatorViewModel.setOperand(btnNum2.text.toString())
+            textViewResult.text = btnNum2.text.toString()
+        }
+        btnNum3.setOnClickListener {
+            Toast.makeText(context, btnNum3.text.toString(), Toast.LENGTH_SHORT).show()
+            calculatorViewModel.setOperand(btnNum3.text.toString())
+            textViewResult.text = btnNum3.text.toString()
+        }
+        btnNum4.setOnClickListener {
+            Toast.makeText(context, btnNum4.text.toString(), Toast.LENGTH_SHORT).show()
+            calculatorViewModel.setOperand(btnNum4.text.toString())
+            textViewResult.text = btnNum4.text.toString()
+        }
+        btnNum5.setOnClickListener {
+            Toast.makeText(context, btnNum5.text.toString(), Toast.LENGTH_SHORT).show()
+            calculatorViewModel.setOperand(btnNum5.text.toString())
+            textViewResult.text = btnNum5.text.toString()
+        }
+        btnNum6.setOnClickListener {
+            Toast.makeText(context, btnNum6.text.toString(), Toast.LENGTH_SHORT).show()
+            calculatorViewModel.setOperand(btnNum6.text.toString())
+            textViewResult.text = btnNum6.text.toString()
+        }
+        btnNum7.setOnClickListener {
+            Toast.makeText(context, btnNum7.text.toString(), Toast.LENGTH_SHORT).show()
+            calculatorViewModel.setOperand(btnNum7.text.toString())
+            textViewResult.text = btnNum7.text.toString()
+        }
+        btnNum8.setOnClickListener {
+            Toast.makeText(context, btnNum8.text.toString(), Toast.LENGTH_SHORT).show()
+            calculatorViewModel.setOperand(btnNum8.text.toString())
+            textViewResult.text = btnNum8.text.toString()
+        }
+        btnNum9.setOnClickListener {
+            Toast.makeText(context, btnNum9.text.toString(), Toast.LENGTH_SHORT).show()
+            calculatorViewModel.setOperand(btnNum9.text.toString())
+            textViewResult.text = btnNum9.text.toString()
+        }
+        btnNumPoint.setOnClickListener {
+            Toast.makeText(context, btnNumPoint.text.toString(), Toast.LENGTH_SHORT).show()
+            calculatorViewModel.setOperand(btnNumPoint.text.toString())
+            textViewResult.text = btnNumPoint.text.toString()
         }
     }
 
--- app/src/main/java/com/lessthanthree/anything/ui/calculator/CalculatorViewModel.kt
+++ app/src/main/java/com/lessthanthree/anything/ui/calculator/CalculatorViewModel.kt
@@ -1,7 +1,65 @@
 package com.lessthanthree.anything.ui.calculator
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.lessthanthree.anything.model.Calculator
 
 class CalculatorViewModel : ViewModel() {
     // TODO: Implement the ViewModel
+    private var calculator = Calculator()
+
+    fun calculate(): String? {
+        return calculator.calculate()
+    }
+
+    fun addition() {
+        if (calculator.operandFirst != null
+            && calculator.operandFirst != "")
+            calculator.addition()
+    }
+
+    fun subtraction() {
+        if (calculator.operandFirst != null
+            && calculator.operandFirst != "")
+            calculator.subtraction()
+    }
+
+    fun multiply() {
+        if (calculator.operandFirst != null
+            && calculator.operandFirst != "")
+            calculator.multiply()
+    }
+
+    fun divide() {
+        if (calculator.operandFirst != null
+            && calculator.operandFirst != "")
+            calculator.divide()
+    }
+
+    fun clear() {
+        if (calculator.operandFirst != null
+            && calculator.operandFirst != "")
+            calculator.clear()
+    }
+
+    fun delete() {
+        if (calculator.operandFirst != null
+            && calculator.operandFirst != "")
+            calculator.delete()
+    }
+
+    fun setOperand(value: String?) {
+        if (calculator.operandFirst != null
+            && calculator.operandFirst != ""
+            && calculator.operator != null
+            && calculator.operator != "") {
+                calculator.operandSecond = value
+                Log.d("C", value + " OS " + calculator.operandSecond)
+                Log.d("C", value + " O " + calculator.operator)
+        } else {
+            calculator.operandFirst = value
+            Log.d("C", value + " OF " + calculator.operandFirst)
+        }
+    }
+
 }
