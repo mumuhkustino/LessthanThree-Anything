1b8f927e7ef186df64573f31a611588207447e4e
KKKK <36650689+atikapane@users.noreply.github.com>
2020-05-30 13:04:31 +0700

    Binding Data from DB to Notes List

--- app/src/main/java/com/lessthanthree/anything/adapter/NotesListAdapter.kt
+++ app/src/main/java/com/lessthanthree/anything/adapter/NotesListAdapter.kt
@@ -1,5 +1,6 @@
 package com.lessthanthree.anything.adapter
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,8 +8,18 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.lessthanthree.anything.R
+import com.lessthanthree.anything.model.Note
+import java.text.DateFormat
 
 class NotesListAdapter : RecyclerView.Adapter<NotesListAdapter.ListViewHolder>(){
+
+    var listNotes : List<Note>
+
+    init {
+        listNotes = arrayListOf()
+
+    }
+
     inner class ListViewHolder (itemView : View) : RecyclerView.ViewHolder(itemView) {
         var noteDate : TextView = itemView.findViewById(R.id.tvDateNoteList)
         var noteTitle : TextView = itemView.findViewById(R.id.tvTitleNoteList)
@@ -20,12 +31,26 @@ class NotesListAdapter : RecyclerView.Adapter<NotesListAdapter.ListViewHolder>()
     }
 
     override fun getItemCount(): Int {
-        return 8
+        Log.d("adapter ", "" + listNotes.size)
+        return listNotes.size
     }
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
         holder.itemView.setOnClickListener {
             Toast.makeText(holder.itemView.context, "tester", Toast.LENGTH_SHORT).show()
         }
+
+        Log.d("adapter ", "" + listNotes[position].toString())
+
+        var note = listNotes.get(position)
+        var date = note.date
+
+        holder.noteDate.text = DateFormat.getDateInstance().format(date)
+        holder.noteTitle.text = note.description
+    }
+
+    internal fun setNotes(notes : List<Note>){
+        this.listNotes = notes
+        notifyDataSetChanged()
     }
 }
--- app/src/main/java/com/lessthanthree/anything/ui/notes/NotesListFragment.kt
+++ app/src/main/java/com/lessthanthree/anything/ui/notes/NotesListFragment.kt
@@ -1,10 +1,13 @@
 package com.lessthanthree.anything.ui.notes
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.lessthanthree.anything.R
@@ -12,6 +15,8 @@ import com.lessthanthree.anything.adapter.NotesListAdapter
 import kotlinx.android.synthetic.main.fragment_notes_list.*
 
 class NotesListFragment : Fragment() {
+
+    private lateinit var viewModel: NotesViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,9 +29,22 @@ class NotesListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val notesListAdapter = NotesListAdapter()
+
         rvNotes.setHasFixedSize(true)
         rvNotes.layoutManager = LinearLayoutManager(context)
-        val notesListAdapter = NotesListAdapter()
         rvNotes.adapter = notesListAdapter
+
+        viewModel = ViewModelProvider(this)[NotesViewModel::class.java]
+
+        viewModel.allNotes.observe(viewLifecycleOwner,
+            Observer { list ->
+                notesListAdapter.setNotes(list)
+                for(i in list.indices) {
+                    Log.d("fragment : ", "" + list[i].toString())
+                }
+            })
+
     }
+
 }
